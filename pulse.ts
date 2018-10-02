12ad5ff38c3772c12647d155096e26538ee7817f
designingourtomorrow <42643946+designingourtomorrow@users.noreply.github.com>
2018-10-02 12:31:57 +0100

    Timing changes, algorithm changes
    Moved over to runningTime() from measured elapsed time, to improve IBI accuracy.
    
    Better guesses at what makes a 'pulse' and when we should close down.
    
    Still a lot of cruft in here, some of which looks future-useful.

--- pulse.ts
+++ pulse.ts
@@ -9,36 +9,44 @@
 //% weight=60 color=#444A84 icon="\uf051" block="DOT Pulse"
 namespace amped {
 
-    let sampleIntervalMS = 10
+    let sampleIntervalMS = 20
 
     let rate: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let values: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     let inputPin: AnalogPin = AnalogPin.P0
     let QS: boolean = false
-    let BPM = 5
+    let BPM = 0
     let IBI = 600                                     // InterBeat Interval, ms
     let pulse = false
-    let sampleCounter: number = 0
-    let lastBeatTime: number = 0
+    let lastBeatTime: number = input.runningTime()
     let Peak: number = 512
     let Trough: number = 512
-    let threshSetting: number = 550
+    let threshSetting: number = 0
     let thresh: number = threshSetting
-    let amp = 100 // amplitude is 1/10 of input range.  May alter for 3.3V
+    let amp = 90 // amplitude is 1/10 of input range.  May alter for 3.3V
     let firstBeat = true  // looking for the first beat
     let secondBeat = false // not yet looking for the second beat in a row
     let signal: number = 0
+    let rising: boolean = false
+    let rateTotal: number = 0                           // tracks time taken for IBI
+    let runningTotal: number = 0                        // } We use these to track if we are rising or falling
+    let lastTotal: number = 0                           // }
+
 
     //% block="set input pin to $pin"
-    export function setPinNumber(pin:AnalogPin) {
+    export function setPinNumber(pin: AnalogPin) {
         inputPin = pin
     }
-
-    let fadeLevel = 0;     // graphing up to 25, but we're dark
 
     //% block
     export function getSampleInterval() {
         return sampleIntervalMS
+    }
+
+    //% block
+    export function setSampleInterval(value: number) {
+        sampleIntervalMS = value
     }
 
     function mapPinToSample(value: number) {
@@ -46,8 +54,8 @@ namespace amped {
     }
 
     //% block="current value"
-    export function getLatestSample() {
-        return signal
+    export function getLatestSample() {   // We're currently sampling from the end for no reason I can really recall...
+        return values[values.length - 2]
     }
 
     /**
@@ -58,12 +66,20 @@ namespace amped {
         return BPM
     }
 
-    function getIBI() {
-        return IBI
+    /**
+     * get amplitude of signal (how big from highest to lowest)
+     */
+    //% block="amp"
+    export function getAmp() {
+        return amp
     }
 
-    function getPulseAmplitude() {
-        return amp
+    /**
+    * show Inter-Beat Interval
+    */
+    //% block="IBI"
+    export function getIBI() {
+        return IBI
     }
 
     function getLastBeatTime() {
@@ -86,12 +102,81 @@ namespace amped {
     //% block="read (and save) value"
     export function readNextSample() {
         // assume that reading is atomic, perfect, complete, and does not get in the way of other things
-        //signal = mapPinToSample(pins.analogReadPin(inputPin))
-        signal = pins.analogReadPin(inputPin)
+        moveThresh(values.shift())
+        let value: number = Math.round(pins.analogReadPin(inputPin))   // !! magic number - 500 is the usual floating voltage, and offsetting here stops overflow spikes in thresh calculation
+        moveThresh(value)
+        values.push(value)
+        signal = value
     }
 
-    function getSampleCounter() {
-        return sampleCounter
+    function updateRunningTotal() {
+        runningTotal = Math.round(values[values.length - 2] + (values[values.length - 3]))// + values[values.length - 1]))
+    }
+
+    function calculateRunningTotal() {  // used if we believe that the running total is out of synch
+        lastTotal = runningTotal
+        updateRunningTotal()
+    }
+
+    /**
+     * set threshold
+     */
+    function setThresh(newThresh: number): void {
+        thresh = newThresh
+    }
+
+    function moveThresh(shift: number) {
+        if (shift == 0)
+        { return }
+        thresh += Math.round(shift / 10)           // this gives us a better idea of amplitude - how big the pulsebeat is
+    }
+
+    function findAmp() {
+        return Math.round(((Peak + Trough) / 2) + Trough)
+    }
+
+    function findMax(array: number[]) {                            // !! highly inefficient, but we deal with that once it works
+        let peak: number = array[0]
+        for (let i: number = 1; i < array.length; i++) {
+            if (array[i] > peak) {
+                peak = array[i]
+            }
+        }
+        return peak
+    }
+
+    function findMin(array: number[]) {                           // !! highly inefficient, but we deal with that once it works
+        let trough: number = array[0]
+        for (let i: number = 1; i < array.length; i++) {
+            if (array[i] < trough) {
+                trough = array[i]
+            }
+        }
+        return trough
+    }
+
+    function setAmp(value: number) {
+        amp = value
+    }
+
+    function pulseOver() {
+        pulse = false
+        rising = false
+        setAmp(findAmp())
+        Peak = thresh
+        Trough = thresh
+        rate[9] = IBI
+        rateTotal += rate[9]
+        rateTotal /= 10                                 // this gives us an average, so we avoid spikes
+        BPM = Math.round(60000 / rateTotal)             // 60,000ms = 60secs
+        QS = true                                       // Quantified Self (detected a beat!)
+
+    }
+
+    function newPulse(N: number) {
+        pulse = true
+        IBI = N
+        lastBeatTime = input.runningTime()
     }
 
     /**
@@ -99,31 +184,26 @@ namespace amped {
      */
     //% block="process current value"
     export function processLatestSample() {
-        sampleCounter += sampleIntervalMS
-        let N = sampleCounter - lastBeatTime          // N is a time interval
-
-        // here we can fade the graph in/out if we want.
+        let N: number = input.runningTime() - lastBeatTime          // N is a time interval
+        calculateRunningTotal()                                     // also updates last total
 
         // find the peak/trough of the pulse wave.
-        if (signal < thresh && N > (IBI / 5) * 3) {      // avoid double beats by waiting 3/5 of time since last
-            if (signal < Trough) {
-                Trough = signal                             // finding the bottom of the trough
+        let Max = findMax(values)                             //!! Here, we die of inefficiency and sorrow
+        let Min = findMin(values)
+        setAmp(Max - Min)
+
+        if (N > 250) {                                              // more than a quarter of a second since the last pulse
+            if (runningTotal > lastTotal) { // The pulse jumps up pretty quickly.
+                rising = true
             }
-        }
-        if (signal > thresh && signal > Peak) {
-            Peak = signal                                 // keep track of the highest point in the wave
-        }
 
-        if (N > 250) {
-            if ((signal > thresh) && (pulse == false) && (N > (IBI / 5) * 3)) {
-                pulse = true
-                IBI = sampleCounter - lastBeatTime
-                lastBeatTime = sampleCounter
-
+            if ((pulse == false) && (N > (IBI / 5) * 3) && rising == true && amp > 100) {
+                newPulse(N)                                  // sets pulse to true, sets IBI to N, moves lastBeatTime to input.runningTime
                 if (secondBeat) {
                     secondBeat = false                      // We are no longer looking for the second beat
                     for (let i = 0; i < 10; i++) {
                         rate[i] = IBI                       // Seed the running total to take a quick stab at the BPM
+
                     }
                 }
 
@@ -134,41 +214,37 @@ namespace amped {
                     return   // bug out for the moment...
                 }
 
-                let runningTotal: number = 0
-                for (let i = 0; i < 9; i++) {
-                    rate[i] = rate[i + 1]               // we could do this with shift, but we'd still have to do the next line...
-                    runningTotal += rate[i]
+                for (let i = 0; i < rate.length; i++) {
+                    rate[i] = rate[i + 1]                   // we could do this with shift, but we'd still have to do the next line...
+                    rateTotal += rate[i]
+                    let a: number = values[i]
+                    if (a > Peak)
+                    { Peak = a }
+                    if (a < Trough)
+                    { Trough = a }
                 }
 
-                rate[9] = IBI
-                runningTotal += rate[9]
-                runningTotal /= 10                      // this gives us an average, so we avoid spikes
-                BPM = Math.round(60000 / runningTotal)             // 60,000ms = 60secs
-                QS = true                               // Quantified Self (detected a beat!)
-
-                // if we were going to use LEDs we could graph them here
             }
         }
 
-        if (signal < thresh && pulse == true) {  // values are going down, so the beat is over
-            pulse = false
-            amp = Peak - Trough
-            thresh = (amp / 2) + Trough           // this gives us a better idea of amplitude - how big the pulsebeat is
-            Peak = thresh
-            Trough = thresh
+        if (lastTotal > runningTotal && (pulse == true)) {  // values are going down, so the beat is over
+            pulseOver()
         }
+
         if (N > 2500) {                             // 2.5 seconds without a beat means we need to reset
-            thresh = threshSetting
+            thresh = (Peak + Trough) / 2
             Peak = 512
             Trough = 512
-            lastBeatTime = sampleCounter
+            lastBeatTime = input.runningTime()
             firstBeat = true                        // look once more for the first beat
             secondBeat = false
             QS = false
             BPM = 0
             IBI = 600
             pulse = false
-            amp = 100
+            amp = 90
+            calculateRunningTotal()                 // Might as well.  We're not doing anything else.
         }
+        // moveThresh(0)                              // We always want to move the threshold after a beat, but probably not brutally
     }
 }
